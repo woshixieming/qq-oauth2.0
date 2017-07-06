456744477444af8005ec7e8787449bae40d8c620
xieming <xieming@gtshidai.com>
2017-07-06 16:56:08 +0800

    no message

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/6
+ * Time: 16:52
+ */
+namespace woshixm\qqlogin;
+
+use woshixm\qqlogin\qqClinet;
+
+class index {
+
+    public function index(){
+
+        new qqClinet();
+    }
+}
